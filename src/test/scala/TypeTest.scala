b6c420e13deb66fcc9f3b2697f25eac0a3cc2173
muszynskim <MuszynskiM@quindell.com>
2014-11-15 17:02:02 +0000

    type test

---
+++ src/test/scala/TypeTest.scala
@@ -0,0 +1,42 @@
+import org.junit.Test
+import org.junit.Assert._
+
+/**
+ * Created by mm on 27/04/2014.
+ */
+class TypeTest {
+
+  @Test
+  def basicTypeTest(): Unit = {
+
+    case class MyClass(val x:Int)
+
+    val i1:MyClass = MyClass(3)
+
+    type A = MyClass
+
+    val i2:A = MyClass(4)
+
+    /**
+     * Section 29.6 PiS p.648
+     * The ".type" on the end means that this is a singleton type.
+     * A singleton type is very specific and holds only one object, whichever
+     * object is refered to by i1.
+     */
+    val i3:i1.type = i1
+    // i3 = i2 // this won't compile as the type of i3 is a singleton type i1.type
+
+    /**
+     * The following does not compile:
+     */
+    //val i4:i2.type = i1
+
+    println(i2)
+    println(MyClass(5))
+    println(i3)
+
+    assertEquals(i1,i3)
+
+  }
+
+}
