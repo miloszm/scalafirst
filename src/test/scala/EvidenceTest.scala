9a76a9f23b3fa71732cfc49d909e0378adcbac3c
muszynskim <MuszynskiM@quindell.com>
2014-11-15 13:46:20 +0000

    added implicit not found annotation test

--- src/test/scala/EvidenceTest.scala
+++ src/test/scala/EvidenceTest.scala
@@ -2,11 +2,14 @@
  * Created by mm on 15/03/2014.
  *
  * Tests for section 21.8 Evidence
+ *                   21.9 The @implicitNotFound Annotation
  *
  */
 
 import org.junit.Test
 import org.junit.Assert._
+
+import scala.annotation.implicitNotFound
 
 
 class EvidenceTest {
@@ -20,9 +23,19 @@ class EvidenceTest {
     println(str)
     assertEquals("checking <:< relation between types String and AnyRef did not work","111", str)
 
+  }
+
+  @Test
+  def testSmallerColonSmallerBasic_ImplicitNotFoundAnnotation = {
+
+    @implicitNotFound(msg = "Can't prove that ${A} is subtype of ${B}")
+    def doSth[A,B](sth: A)(implicit evidence: A <:< B):String = sth.toString
+
     /**
      * Note - the following won't compile as
      * String does not have a "<:<" relation with Int, i.e., "is subtype of"
+     * Compilation error will be as defined in the @implicitNotFound annotation
+     * To see it uncomment out this code and try to compile
      */
     //doSth[String, Int]("222")
 
@@ -49,6 +62,13 @@ class EvidenceTest {
      * String does not have a "=:=" relation with AnyRef, i.e., "is equal to"
      */
     //doSth[String, AnyRef]("111")
+
+  }
+
+  @Test
+  def testEqualsColonEqualsBasicImplicitly = {
+
+    assertTrue(implicitly[String =:= String].isInstanceOf[Function1[String,String]])
 
   }
 
