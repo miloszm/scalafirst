345a4c8b3a3ba469c1947606b28457f351736336
muszynskim <MuszynskiM@quindell.com>
2014-11-15 16:14:02 +0000

    can build from continued

--- src/main/scala/Hello.scala
+++
@@ -1,8 +0,0 @@
-/**
- * Created by mm on 12/03/2014.
- */
-object Hello extends App {
-
-  println("Hello");
-
-}
--- src/test/scala/CanBuildFromTest.scala
+++ src/test/scala/CanBuildFromTest.scala
@@ -9,7 +9,10 @@
 import org.junit.Test
 import org.junit.Assert._
 
+import scala.collection.generic.{CanBuildFrom, GenTraversableFactory}
 import scala.collection.immutable.BitSet
+import scala.collection.mutable.Builder
+import scala.collection.mutable.Buffer
 import scala.collection.mutable.ArrayBuffer
 
 class CanBuildFromTest {
@@ -26,13 +29,10 @@ class CanBuildFromTest {
 
     println(i1)
 
-    assertTrue(i1.isInstanceOf[scala.collection.generic.BitSetFactory])
-
     val i2 = implicitly[scala.collection.generic.CanBuildFrom[Set[_], Int, Set[Int]]]
 
     println(i2)
 
-    assertTrue(i1.isInstanceOf[scala.collection.generic.GenSetFactory])
   }
 
   /**
@@ -49,6 +49,43 @@ class CanBuildFromTest {
     val a:Array[Int] = builder.result()
 
     a.foreach(println)
+  }
+
+  /**
+   * Section 21.10 CanBuildFrom Demystified (SftI)
+   *
+   * Each collection provides an implicit CanBuildFrom object in its companion object.
+   */
+  @Test
+  def basicCanBuildFromTest = {
+
+    val i1 = implicitly[scala.collection.generic.CanBuildFrom[ArrayBuffer[Int], Int, ArrayBuffer[Int]]]
+
+    println(i1)
+
+    val i2 = implicitly[scala.collection.generic.CanBuildFrom[Buffer[Int], Int, Buffer[Int]]]
+
+    println(i2)
+
+    /**
+     * we obtain a builder factory of a Buffer
+     */
+    val bf:CanBuildFrom[Buffer[Int], Int, Buffer[Int]] = Buffer.canBuildFrom[Int]
+
+    /**
+     * we obtain a builder from builder factory
+     */
+    val b:Builder[Int, Buffer[Int]] = bf.apply
+
+    println(b)
+
+    /**
+     * the builder is ArrayBuffer is this case
+     * class ArrayBuffer[A] ... with Builder[A, ArrayBuffer[A]]
+     * implements +=, clear, result, mapResult
+     */
+    assertTrue(b.isInstanceOf[ArrayBuffer[Int]])
+
   }
 
 
