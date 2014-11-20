663afbf29bde33615d0afcc78dc6424aa9302c8b
muszynskim <MuszynskiM@quindell.com>
2014-11-20 13:22:31 +0000

    added comment

--- src/main/scala/com/mimu/implicits/MimuString.scala
+++ src/main/scala/com/mimu/implicits/MimuString.scala
@@ -14,6 +14,12 @@ class MimuString(val str:String) {
 
 object MimuString {
   def apply(str:String) = new MimuString(str)
+
+  /**
+   * note - after Scala 2.10 it is possible to declare a class implicit, so going through
+   * conversion function like the one below is not needed
+   * we could declare class MimuString implicit and that would suffice
+   */
   implicit def string2MimuString(str:String) = new MimuString(str)
   implicit val d = Delimiters("<<<", ">>>")
 }
