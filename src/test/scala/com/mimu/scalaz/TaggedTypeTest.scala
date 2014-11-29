3e9709d4e5c372c525996275c631fbb088aa5bed
muszynskim <MuszynskiM@quindell.com>
2014-11-29 12:07:09 +0000

    more tagged type tests

--- src/test/scala/com/mimu/scalaz/TaggedTypeTest.scala
+++ src/test/scala/com/mimu/scalaz/TaggedTypeTest.scala
@@ -49,6 +49,62 @@ class TaggedTypeTest {
 
   }
 
+  /**
+   * from: http://timperrett.com/2012/06/15/unboxed-new-types-within-scalaz7
+   * this is really cool
+   */
+  @Test
+  def monoidWithTaggedTypeTest(): Unit ={
+
+    import Tags._
+
+    println(3 |+| 5)
+    println(Multiplication(3) |+| Multiplication(5))
+
+    assertEquals(8, 3 |+| 5)
+    assertEquals(15, Multiplication(3) |+| Multiplication(5))
+  }
+
+  /**
+   * eed3si9n kilogram example
+   */
+  @Test
+  def test_eed3si9n_kilogram_example(): Unit ={
+
+    sealed trait KiloGram
+
+    def KiloGram[A](a: A): A @@ KiloGram = Tag[A, KiloGram](a)
+
+    type KiloGramT = Int @@ KiloGram
+
+    val mass = KiloGram(20.0)
+
+    val m = Tag.unwrap(mass)
+
+    println(m)
+
+    /**
+     * Removes tag
+     */
+    val um = Tag.unsubst[Double, Id, KiloGram](mass)
+
+    println(um)
+
+    def doSthWithKilogram(a:Int @@ KiloGram): Unit ={
+
+    }
+
+    def doSthWithKilogram2(a:KiloGramT): Unit ={
+
+    }
+
+    //doSthWithKilogram(20) // luckily it does not compile
+    doSthWithKilogram(KiloGram(20)) // compiles
+    //doSthWithKilogram2(20) // luckily it does not compile
+    doSthWithKilogram2(KiloGram(20)) // compiles
+
+  }
+
 
 
 }
