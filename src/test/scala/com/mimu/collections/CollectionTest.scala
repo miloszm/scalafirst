ec6814f6eefc42d3e5fe938188b79c589e7dfae6
muszynskim <MuszynskiM@quindell.com>
2014-12-05 13:50:12 +0000

    added tests for SC collections sections 10-13

--- src/test/scala/com/mimu/collections/CollectionTest.scala
+++ src/test/scala/com/mimu/collections/CollectionTest.scala
@@ -120,6 +120,153 @@ class CollectionTest {
     }
   }
 
+  @Test
+  def testCollections_SC_10_10(): Unit = {
+
+    val fruits = Traversable("apple", "banana", "peach")
+    val fruitsA = Array("apple", "banana", "peach")
+    val fruitsB = IndexedSeq("apple", "banana", "peach")
+
+    /**
+     *
+     * zipWithIndex:
+     *
+     * implementation in: trait IndexedSeqOptimized
+     *
+     * declared in IterableLike as
+     *
+     *     def zipWithIndex[A1 >: A, That](implicit bf : scala.collection.generic.CanBuildFrom[Repr, scala.Tuple2[A1, scala.Int], That]) : That
+     *
+     */
+
+    //for ((elem, count) <- fruits.zipWithIndex){ // won't compile as it is not a member of Traversable
+    for ((elem, count) <- fruitsA.zipWithIndex){
+      println(s"$count $elem (Array) ${fruitsA.getClass()}")
+    }
+    for ((elem, count) <- fruitsB.zipWithIndex){
+      println(s"$count $elem (IndexedSeq) ${fruitsB.getClass()}")
+    }
+
+    /**
+     * with view
+     */
+    for ((elem, count) <- fruitsB.view.zipWithIndex){
+      println(s"$count $elem (with view)")
+    }
+
+    /**
+     * zip with Stream from 1
+     * (really cool)
+     */
+    for ((elem, count) <- fruitsB zip (Stream from 1)){
+      println(s"$count $elem (index obtained from Stream)")
+    }
+
+    val r = for ((elem, count) <- fruitsB zip (Stream from 1)) yield (elem,count)
+    println(s"$r + is of class ${r.getClass}")
+
+  }
+
+  @Test
+  def testCollections_SC_10_11(): Unit = {
+
+    val l = List("a", "b", "c")
+    val zwi = l.zipWithIndex
+    println(zwi)
+
+    l.zipWithIndex.foreach {
+      d => println(s"${d._2} is ${d._1}")
+    }
+
+  }
+
+  /**
+   *
+   */
+  @Test
+  def testCollections_SC_10_99(): Unit = {
+
+  }
+
+  /**
+   * using iterators
+   *
+   * type Iterator[+A] = scala.collection.Iterator[A]
+   *
+   * trait Iterator[+A] extends scala.AnyRef with scala.collection.TraversableOnce[A]
+   */
+  @Test
+  def testCollections_SC_10_12(): Unit = {
+    val it = Iterator(1,2,3)
+    println(it.getClass) // scala.collection.IndexedSeqLike$Elements
+
+    it.foreach(println)
+    it.foreach(println) // this won't print anything as iterator is exhausted
+
+    val ita = Iterator(4,5,6).toArray
+    println(ita.getClass()) // class [I
+    ita.foreach(println)
+
+    val itb = Iterator(7,8,9).toStream
+    println(itb.getClass()) // scala.collection.immutable.Stream$Cons
+    itb.foreach(println)
+
+  }
+
+  /**
+   * transforming one collection into another with for/yield
+   *
+   * In general, the collection type that's returned by a for comprehension
+   * will be the same type that you begin with
+   */
+  @Test
+  def testCollections_SC_10_13(): Unit = {
+
+    val f = Vector("apple", "banana", "peach")
+
+    val pairs = for (ff <- f) yield (ff, ff.length)
+    println(pairs)
+    println(pairs.getClass())  // scala.collection.immutable.Vector
+    println(pairs.getClass().getComponentType)  // null - does it work only for Arrays ?
+
+    val fa = Array("apple", "banana", "peach")
+    val pairsa = for (ff <- fa) yield (ff, ff.length)
+    println(pairsa)
+    println(pairsa.getClass())  // [Lscala.Tuple2;
+    println(pairsa.getClass().getComponentType)  // scala.Tuple2
+
+  }
+
+  /**
+   * transforming one collection to another with map
+   */
+  @Test
+  def testCollections_SC_10_14(): Unit = {
+    val helpers = Vector("John", "Kate", "Sue")
+    val caps = helpers map {e => e.toUpperCase()}
+    println(caps.getClass)  // Vector
+    println(caps.getClass().getComponentType)  // null
+    val lengths = helpers map { e => e.length}
+    println(lengths.getClass)  // Vector
+    println(lengths.getClass().getComponentType)  // null
+
+    println("HAL".map((c:Char) => (c.toByte+1).toChar))
+
+    val f = Vector("apple", "banana", "peach")
+    val newFruits = f.map( f =>
+      if (f.length < 6) f.toUpperCase()
+    )
+    println(newFruits)
+    assertTrue(newFruits.contains(())) // contains Unit ()
+  }
+
+
+
+
+
+
+
+
 
 
 
