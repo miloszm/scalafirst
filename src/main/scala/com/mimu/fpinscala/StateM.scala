2687bd403819a9e4aedbb3dfe950b3f3e5ef0594
muszynskim <MuszynskiM@quindell.com>
2016-08-25 19:59:44 +0100

    ex. 6.11

--- src/main/scala/com/mimu/fpinscala/StateM.scala
+++ src/main/scala/com/mimu/fpinscala/StateM.scala
@@ -11,6 +11,12 @@ object StateM {
   def sequence[S,A](fs:List[StateM[S,A]]): StateM[S,List[A]] = {
     fs.foldRight(unit[S,List[A]](List[A]()))((x, y) => x.map2(y)((a,b) => a :: b))
   }
+  def get[S]: StateM[S,S] = StateM(s => (s,s))
+  def set[S](s:S):StateM[S,Unit] = StateM(_ => ((), s))
+  def modify[S](f:S => S): StateM[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
 
 case class StateM[S, +A](run: S => (A,S)) {
---
+++ src/main/scala/com/mimu/fpinscala/ch6/CandyDispenser.scala
@@ -0,0 +1,77 @@
+package com.mimu.fpinscala.ch6
+
+import com.mimu.fpinscala.StateM
+import com.mimu.fpinscala.StateM.sequence
+
+/**
+  * 6.11
+  */
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+  case class Machine(locked:Boolean, candies:Int, coins:Int) {
+
+    def simulateMachine(inputs: List[Input]): StateM[Machine, List[(Int, Int)]] = {
+
+      val l = for {
+        i <- inputs
+      }
+      yield {
+        i match {
+          case Coin => coin
+          case Turn => turn
+        }
+      }
+
+      val x = sequence(l)
+      x
+
+    }
+
+    def coin:StateM[Machine, (Int, Int)] = StateM(m => {
+      if (m.candies > 0){
+        ((m.coins+1, m.candies), Machine(false, m.candies, m.coins+1))
+      }
+      else {
+        ((m.coins, m.candies), Machine(m.locked, m.candies, m.coins))
+      }
+    })
+
+    def turn:StateM[Machine, (Int, Int)] = StateM(m => {
+      if (m.candies > 0 && !m.locked){
+        ((m.coins, m.candies-1), Machine(true, m.candies-1, m.coins))
+      }
+      else {
+        ((m.coins, m.candies), Machine(m.locked, m.candies, m.coins))
+      }
+    })
+
+  }
+
+  object Candy {
+
+    def simulateMachine(inputs: List[Input]): StateM[Machine, List[Unit]] = {
+
+      val x: List[StateM[Machine, Unit]] = inputs.map(i => StateM.modify((s: Machine) => (i, s) match {
+        case (_, Machine(_, 0, _)) => s
+        case (Coin, Machine(false, _, _)) => s
+        case (Turn, Machine(true, _, _)) => s
+        case (Coin, Machine(true, candy, coin)) => Machine(false, candy, coin + 1)
+        case (Turn, Machine(false, candy, coin)) => Machine(true, candy - 1, coin)
+      }))
+      val a: StateM[Machine, List[Unit]] = sequence(x)
+      a
+    }
+  }
+
+object CandyDispenser extends App {
+
+
+  val f = Machine(true, 10, 0).simulateMachine(List(Coin,Turn,Coin,Turn))
+  println(f.run(Machine(true, 10, 0)))
+
+  val ff = Candy.simulateMachine(List(Coin,Turn,Coin,Turn))
+  println(ff.run(Machine(true, 10, 0)))
+
+}
